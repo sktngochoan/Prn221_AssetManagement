66cb594e659edfed1faf0bd89cdd7775606c8e50
Nguyen Phong Hao <haonphe153346@fpt.edu.vn>
2023-03-22 23:12:17 +0700

    Asset Borrowing

--- AssetManagement/AssetManagement/Models/Asset.cs
+++ AssetManagement/AssetManagement/Models/Asset.cs
@@ -5,6 +5,11 @@ namespace AssetManagement.Models
 {
     public partial class Asset
     {
+        public Asset()
+        {
+            BorrowingAssets = new HashSet<BorrowingAsset>();
+        }
+
         public int Id { get; set; }
         public int? CategoryId { get; set; }
         public string? AssetName { get; set; }
@@ -13,5 +18,6 @@ namespace AssetManagement.Models
         public string? Specification { get; set; }
 
         public virtual Category? Category { get; set; }
+        public virtual ICollection<BorrowingAsset> BorrowingAssets { get; set; }
     }
 }
