15ff38269b8f4d473d7893a833d052374eaf0625
Trần Xuân Sơn <sowndev@gmail.com>
2023-03-23 23:59:09 +0700

    CRUD Assets, CRUD Users

---
+++ AssetManagement/AssetManagement/Pages/Admin/Users/List.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AssetManagement.Models;
+
+namespace AssetManagement.Pages.Admin.Users
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AssetManagement.Models.StockManagemnetContext _context;
+
+        public IndexModel(AssetManagement.Models.StockManagemnetContext context)
+        {
+            _context = context;
+        }
+
+        public IList<User> User { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Users != null)
+            {
+                User = await _context.Users
+                .Include(u => u.Role).ToListAsync();
+            }
+        }
+    }
+}
