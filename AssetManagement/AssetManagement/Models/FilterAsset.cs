fc634aa9a0be8c8106f1629ebe1d2a9c3a0c4a1a
unknown <hoannnhe151308@fpt.edu.vn>
2023-03-23 17:05:36 +0700

    task asset management

---
+++ AssetManagement/AssetManagement/Models/FilterAsset.cs
@@ -0,0 +1,10 @@
+﻿namespace AssetManagement.Models
+{
+    public class FilterAsset
+    {
+        public int Id { get; set; }
+        public int status { get; set; }
+        public string keyWord { get; set; }
+        public int page { get; set; }
+    }
+}
