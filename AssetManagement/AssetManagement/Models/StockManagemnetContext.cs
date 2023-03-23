66dd2a5433521dfb15dea80b4c8f2b7b81fe0701
Trần Xuân Sơn <sowndev@gmail.com>
2023-03-23 21:22:50 +0700

    Borrow + return

--- AssetManagement/AssetManagement/Models/StockManagemnetContext.cs
+++ AssetManagement/AssetManagement/Models/StockManagemnetContext.cs
@@ -21,13 +21,14 @@ namespace AssetManagement.Models
         public virtual DbSet<Category> Categories { get; set; } = null!;
         public virtual DbSet<Role> Roles { get; set; } = null!;
         public virtual DbSet<User> Users { get; set; } = null!;
+      
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=DESKTOP-BQIUTTV\\SQLEXPRESS;database=StockManagemnet;Integrated security=true");
+                optionsBuilder.UseSqlServer("server=LOCALHOST;database=StockManagemnet;Integrated security=true");
             }
         }
 
